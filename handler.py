266126205cb83510bd7e6ea1c1a07ee2699ae454
migueleichler <migueleichler23@gmail.com>
2018-08-19 14:24:00 -0300

    Lambda function receiving s3 object key and saving in table from DynamoDb

--- handler.py
+++ handler.py
@@ -1,11 +1,21 @@
 # coding=utf-8
-
 import boto3
 
-dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
 
 def extractMetadata(event, context):
-    print("Hello World")
+    dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
+    table = dynamodb.Table('images')
+
+    record = event['Records'][0]
+    s3objectkey = record['s3']['object']['key']
+
+    table.put_item(
+        Item={
+            's3objectkey': s3objectkey,
+            'body': 'teste'
+        }
+    )
+
 
 def getMetadata(event, context):
     print("Hello World")
--- tests.py
+++ tests.py
@@ -1,17 +1,69 @@
 import unittest
 import boto3
-from moto import mock_s3
+from moto import mock_s3, mock_dynamodb2
 from handler import extractMetadata
 
 
 class TestHandlerFunctions(unittest.TestCase):
 
-    @mock_s3
     def setUp(self):
-        conn = boto3.resource('s3', region_name='us-east-1')
-        conn.create_bucket(Bucket='teste')
+        self.bucket = 'teste'
+        self.s3objectkey = 'image-001.jpeg'
+        self.table = 'images'
 
-        self.event = {}
+        self.event = {
+            "Records": [
+                {
+                    "s3": {
+                        "object": {
+                            "key": self.s3objectkey,
+                        },
+                    },
+                }
+            ]
+        }
 
+    def set_up_dynamodb(self):
+        dynamodb = boto3.client('dynamodb', region_name='us-east-1')
+        dynamodb.create_table(
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 's3objectkey',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'width',
+                    'AttributeType': 'N',
+                },
+                {
+                    'AttributeName': 'height',
+                    'AttributeType': 'N',
+                }
+            ],
+            KeySchema=[
+                {
+                    'AttributeName': 's3objectkey',
+                    'KeyType': 'HASH'
+                },
+            ],
+            TableName=self.table,
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 1,
+                'WriteCapacityUnits': 1
+            }
+        )
+
+        return dynamodb
+
+    @mock_dynamodb2
     def test_extractMetadata(self):
+        dynamodb = self.set_up_dynamodb()
+
         extractMetadata(self.event, None)
+
+        item = dynamodb.get_item(
+            TableName=self.table,
+            Key={'s3objectkey': {'S': self.s3objectkey}}
+        )
+
+        self.assertEqual(item['Item']['s3objectkey']['S'], self.s3objectkey)
