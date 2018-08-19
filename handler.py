e4f2fa47dc47dcc74ca1eab51934e1a4ab900adc
migueleichler <migueleichler23@gmail.com>
2018-08-19 18:22:54 -0300

    Lambda function extractMetadata saving s3 Images metadata in database

--- handler.py
+++ handler.py
@@ -1,18 +1,37 @@
 # coding=utf-8
 import boto3
+from io import BytesIO
+from PIL import Image
+import sys
+
+
+def get_image_metadata(s3_bucket, s3objectkey):
+    s3 = boto3.resource('s3', region_name='us-east-1')
+
+    obj = s3.Object(bucket_name=s3_bucket, key=s3objectkey)
+    obj_body = obj.get()['Body'].read()
+
+    img = Image.open(BytesIO(obj_body))
+    width = img.size[0]
+    height = img.size[1]
+    file_size = sys.getsizeof(img.tobytes())
+
+    return {'width': width, 'height': height, 'file_size': file_size}
 
 
 def extractMetadata(event, context):
-    dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
-    table = dynamodb.Table('images')
-
     record = event['Records'][0]
+    s3_bucket = record['s3']['bucket']['name']
     s3objectkey = record['s3']['object']['key']
 
+    metadata = get_image_metadata(s3_bucket, s3objectkey)
+
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('images')
     table.put_item(
         Item={
             's3objectkey': s3objectkey,
-            'body': 'teste'
+            'body': metadata
         }
     )
 
--- tests.py
+++ tests.py
@@ -2,14 +2,17 @@ import unittest
 import boto3
 from moto import mock_s3, mock_dynamodb2
 from handler import extractMetadata
+from PIL import Image
+import os
 
 
 class TestHandlerFunctions(unittest.TestCase):
 
     def setUp(self):
         self.bucket = 'teste'
-        self.s3objectkey = 'image-001.jpeg'
+        self.s3objectkey = 'image-001'
         self.table = 'images'
+        self.file_name = 'image.png'
 
         self.event = {
             "Records": [
@@ -17,6 +20,9 @@ class TestHandlerFunctions(unittest.TestCase):
                     "s3": {
                         "object": {
                             "key": self.s3objectkey,
+                        },
+                        "bucket": {
+                            "name": self.bucket,
                         },
                     },
                 }
@@ -31,14 +37,6 @@ class TestHandlerFunctions(unittest.TestCase):
                     'AttributeName': 's3objectkey',
                     'AttributeType': 'S'
                 },
-                {
-                    'AttributeName': 'width',
-                    'AttributeType': 'N',
-                },
-                {
-                    'AttributeName': 'height',
-                    'AttributeType': 'N',
-                }
             ],
             KeySchema=[
                 {
@@ -55,9 +53,26 @@ class TestHandlerFunctions(unittest.TestCase):
 
         return dynamodb
 
+    def set_up_s3(self):
+        image = Image.new('RGBA', size=(50, 50), color=(256, 0, 0))
+        image.save(self.file_name, 'PNG')
+
+        s3_conn = boto3.resource('s3', region_name='us-east-1')
+        s3_conn.create_bucket(Bucket=self.bucket)
+        s3 = boto3.client('s3', region_name='us-east-1')
+        s3.upload_file(
+            Bucket=self.bucket,
+            Key=self.s3objectkey,
+            Filename=self.file_name
+        )
+
+        return s3
+
+    @mock_s3
     @mock_dynamodb2
     def test_extractMetadata(self):
         dynamodb = self.set_up_dynamodb()
+        s3 = self.set_up_s3()
 
         extractMetadata(self.event, None)
 
@@ -66,4 +81,15 @@ class TestHandlerFunctions(unittest.TestCase):
             Key={'s3objectkey': {'S': self.s3objectkey}}
         )
 
-        self.assertEqual(item['Item']['s3objectkey']['S'], self.s3objectkey)
+        s3objectkey = item['Item']['s3objectkey']['S']
+        width = item['Item']['body']['M']['width']['N']
+        height = item['Item']['body']['M']['height']['N']
+        file_size = item['Item']['body']['M']['file_size']['N']
+
+        self.assertEqual(s3objectkey, self.s3objectkey)
+        self.assertEqual(width, '50')
+        self.assertEqual(height, '50')
+        self.assertEqual(file_size, '10033')
+
+    def tearDown(self):
+        os.remove(self.file_name)
