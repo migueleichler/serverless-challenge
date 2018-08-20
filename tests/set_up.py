a44257f4c5898e241afdb86dade266077d38ce8e
migueleichler <migueleichler23@gmail.com>
2018-08-19 22:50:59 -0300

    Folder tmp created inside tests directory to temporarily host image file created during unit tests

---
+++ tests/set_up.py
@@ -0,0 +1,44 @@
+from PIL import Image
+import boto3
+import os
+
+
+def set_up_dynamodb(table):
+    dynamodb = boto3.client('dynamodb', region_name='us-east-1')
+    dynamodb.create_table(
+        AttributeDefinitions=[
+            {
+                'AttributeName': 's3objectkey',
+                'AttributeType': 'S'
+            },
+        ],
+        KeySchema=[
+            {
+                'AttributeName': 's3objectkey',
+                'KeyType': 'HASH'
+            },
+        ],
+        TableName=table,
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 1
+        }
+    )
+
+    return dynamodb
+
+
+def set_up_s3(bucket, s3objectkey, file_name):
+    image = Image.new('RGBA', size=(50, 50), color=(256, 0, 0))
+    image.save(file_name, 'PNG')
+
+    s3_conn = boto3.resource('s3', region_name='us-east-1')
+    s3_conn.create_bucket(Bucket=bucket)
+    s3 = boto3.client('s3', region_name='us-east-1')
+    s3.upload_file(
+        Bucket=bucket,
+        Key=s3objectkey,
+        Filename=file_name
+    )
+
+    return s3
---
+++ tests/tests.py
@@ -0,0 +1,60 @@
+import unittest
+from moto import mock_s3, mock_dynamodb2
+from handler import extractMetadata
+from tests.set_up import set_up_dynamodb, set_up_s3
+import os
+
+
+class TestHandlerFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.bucket = 'teste'
+        self.s3objectkey = 'image-001'
+        self.table = 'images'
+        self.file_path = os.path.dirname(os.path.abspath(__file__)) + '/tmp/'
+        self.file_name = self.file_path + 'image.png'
+
+        self.event = {
+            "Records": [
+                {
+                    "s3": {
+                        "object": {
+                            "key": self.s3objectkey,
+                        },
+                        "bucket": {
+                            "name": self.bucket,
+                        },
+                    },
+                }
+            ]
+        }
+
+    @mock_s3
+    @mock_dynamodb2
+    def test_extractMetadata(self):
+        dynamodb = set_up_dynamodb(table=self.table)
+        s3 = set_up_s3(
+                 bucket=self.bucket,
+                 s3objectkey=self.s3objectkey,
+                 file_name=self.file_name
+             )
+
+        extractMetadata(self.event, None)
+
+        item = dynamodb.get_item(
+            TableName=self.table,
+            Key={'s3objectkey': {'S': self.s3objectkey}}
+        )
+
+        s3objectkey = item['Item']['s3objectkey']['S']
+        width = item['Item']['body']['M']['width']['N']
+        height = item['Item']['body']['M']['height']['N']
+        file_size = item['Item']['body']['M']['file_size']['N']
+
+        self.assertEqual(s3objectkey, self.s3objectkey)
+        self.assertEqual(width, '50')
+        self.assertEqual(height, '50')
+        self.assertEqual(file_size, '10033')
+
+    def tearDown(self):
+        os.remove(self.file_name)
